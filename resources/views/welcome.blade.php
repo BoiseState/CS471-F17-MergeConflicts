0760d2557a79b5e6fbd7e2b77701be3a79a22e83
Tyler Wigington <tylerwigington@u.boisestate.edu>
2017-12-02 22:05:38 -0700

    Scaffolded out admin view and auth using laravel command line tool for #52

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,60 +12,63 @@
 
         <body>
         <div class="">
-            {{--@if (Route::has('login'))--}}
-            {{--<div class="top-right links">--}}
-            {{--@auth--}}
-            {{--<a href="{{ url('/home') }}">Home</a>--}}
-            {{--@else--}}
-            {{--<a href="{{ route('login') }}">Login</a>--}}
-            {{--<a href="{{ route('register') }}">Register</a>--}}
-            {{--@endauth--}}
-            {{--</div>--}}
-            {{--@endif--}}
             <nav class="navbar top-menu">
                 <div class="container-fluid">
                     <div class="navbar-header">
                         <a class="navbar-left" href="{{url('http://interfaithsanctuary.org/')}}">
                             <img class="brand" alt="Brand" src="images/Interfaith-Temp-Logo.png">
                         </a>
-
-
                     </div>
+
+                    <div class="collapse navbar-collapse"  id="app-navbar-collapse">
                     <!-- Collect the nav links, forms, and other content for toggling -->
                         <ul class="nav navbar-nav navbar-center">
-
-                            <li><a class="navbar-brand" href="#">Adopt-a-Meal</a></li>
-
+                            <li><a class="navbar-brand" href="/">Adopt-a-Meal</a></li>
                         </ul>
 
-                    <!-- Collect the nav links, forms, and other content for toggling -->
-                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <!-- Collect the nav links, forms, and other content for toggling -->
                         <ul class="nav navbar-nav navbar-right">
-
-                            <li><a href="#">Calendar View</a></li>
-
-                            <li><a href="#">Meal Ideas</a></li>
-
+                            @guest
+                                <li><a href="#">Calendar View</a></li>
+                                <li><a href="#">Meal Ideas</a></li>
+                            @else
+                                <li><a href="#">Calendar View</a></li>
+                                <li><a href="#">Meal Ideas</a></li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                                        {{ Auth::user()->name }} <span class="caret"></span>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a href="{{ route('logout') }}"
+                                               onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                                Logout
+                                            </a>
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                {{ csrf_field() }}
+                                            </form>
+                                        </li>
+                                    </ul>
+                                </li>
+                            @endguest
                         </ul>
-
-                    </div><!-- /.navbar-collapse -->
-
+                    </div>
                 </div>
-
             </nav>
-            <div class="container">
-                <div class="col-md-8 col-md-offset-2 calender">
-                    Place Calender Here
-                    @foreach($events as $event)
+        </div>
+            {{--<div class="container">--}}
+                {{--<div class="col-md-8 col-md-offset-2 calender">--}}
+                    {{--Place Calender Here--}}
+                    {{--@foreach($events as $event)--}}
 
-                        <div>{{$event->start->dateTime}}</div>
-
-
-                    @endforeach
-                </div>
-            </div>
+                        {{--<div>{{$event->start->dateTime}}</div>--}}
 
 
+                    {{--@endforeach--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            @yield('content')
         </div>
         </body>
 </html>
