ba0fcc41ff48f6b95ef4a46d3095324d4ce19052
Tyler Wigington <tylerwigington@u.boisestate.edu>
2017-12-04 14:03:43 -0700

    Updated blade templates to extend the app template, removed register endpoint from being publicly #54

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,57 +15,44 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
+        <nav class="navbar top-menu">
+            <div class="container-fluid">
                 <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                    <a class="navbar-left" href="{{url('http://interfaithsanctuary.org/')}}">
+                        <img class="brand" alt="Brand" src="images/Interfaith-Temp-Logo.png">
                     </a>
                 </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;
+                        <li><a class="navbar-brand" href="/">Adopt-a-Meal</a></li>
                     </ul>
 
-                    <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
                         @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
+                            <li><a href="#">Calendar View</a></li>
+                            <li><a href="#">Meal Ideas</a></li>
+                            @else
+                                <li><a href="#">Calendar View</a></li>
+                                <li><a href="#">Meal Ideas</a></li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                                        {{ Auth::user()->name }} <span class="caret"></span>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a href="{{ route('logout') }}"
+                                               onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                                Logout
+                                            </a>
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                {{ csrf_field() }}
+                                            </form>
+                                        </li>
+                                    </ul>
+                                </li>
+                                @endguest
                     </ul>
                 </div>
             </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,76 +1,17 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Adopt-A-Meal</title>
-        <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    </head>
+@section('content')
+<div class="container">
+    <div class="col-md-8 col-md-offset-2 calender">
+        Place Calender Here
+        @foreach($events as $event)
 
-        <body>
-        <div class="">
-            <nav class="navbar top-menu">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <a class="navbar-left" href="{{url('http://interfaithsanctuary.org/')}}">
-                            <img class="brand" alt="Brand" src="images/Interfaith-Temp-Logo.png">
-                        </a>
-                    </div>
-
-                    <div class="collapse navbar-collapse"  id="app-navbar-collapse">
-                    <!-- Collect the nav links, forms, and other content for toggling -->
-                        <ul class="nav navbar-nav navbar-center">
-                            <li><a class="navbar-brand" href="/">Adopt-a-Meal</a></li>
-                        </ul>
-
-                        <!-- Collect the nav links, forms, and other content for toggling -->
-                        <ul class="nav navbar-nav navbar-right">
-                            @guest
-                                <li><a href="#">Calendar View</a></li>
-                                <li><a href="#">Meal Ideas</a></li>
-                            @else
-                                <li><a href="#">Calendar View</a></li>
-                                <li><a href="#">Meal Ideas</a></li>
-                                <li class="dropdown">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                        {{ Auth::user()->name }} <span class="caret"></span>
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li>
-                                            <a href="{{ route('logout') }}"
-                                               onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                                Logout
-                                            </a>
-                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                                {{ csrf_field() }}
-                                            </form>
-                                        </li>
-                                    </ul>
-                                </li>
-                            @endguest
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
-            {{--<div class="container">--}}
-                {{--<div class="col-md-8 col-md-offset-2 calender">--}}
-                    {{--Place Calender Here--}}
-                    {{--@foreach($events as $event)--}}
-
-                        {{--<div>{{$event->start->dateTime}}</div>--}}
+            <div>{{$event->start->dateTime}}</div>
 
 
-                    {{--@endforeach--}}
-                {{--</div>--}}
-            {{--</div>--}}
-            @yield('content')
-        </div>
-        </body>
-</html>
+        @endforeach
+    </div>
+</div>
+@endsection
 
 
