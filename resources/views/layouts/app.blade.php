46e893528232c05defaac338f8116271ef829e7d
Tyler Wigington <tylerwigington@u.boisestate.edu>
2017-12-06 14:21:58 -0700

    Fixed broken javascript. #54

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,36 +32,36 @@
                         @guest
                             <li><a href="#">Calendar View</a></li>
                             <li><a href="#">Meal Ideas</a></li>
-                            @else
-                                <li><a href="#">Calendar View</a></li>
-                                <li><a href="#">Meal Ideas</a></li>
-                                <li class="dropdown">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                        {{ Auth::user()->name }} <span class="caret"></span>
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li>
-                                            <a href="{{ route('logout') }}"
-                                               onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();">
-                                                Logout
-                                            </a>
-                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                                {{ csrf_field() }}
-                                            </form>
-                                        </li>
-                                    </ul>
-                                </li>
-                                @endguest
+                        @else
+                            <li><a href="#">Calendar View</a></li>
+                            <li><a href="#">Meal Ideas</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endguest
                     </ul>
                 </div>
             </div>
         </nav>
-
         @yield('content')
     </div>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+    @yield('scripts')
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,35 @@
 @extends('layouts.app')
 
+@section('scripts')
+    <script>
+      $(document).ready(function() {
+        // page is now ready, initialize the calendar...
+        $('#calendar').fullCalendar({
+          showNonCurrentDates: false,
+          contentHeight : "auto",
+          height: 'parent' +80 ,
+          aspectRatio: 1.5,
+          themeSystem: 'bootstrap3'
+          // put your options and callbacks here
+        });
+      });
+    </script>
+@endsection
+
 @section('content')
 <div class="container">
-    <div class="col-md-8 col-md-offset-2 calender">
-        Place Calender Here
-        @foreach($events as $event)
+    <div class="col-md-10 col-md-offset-2 calendar">
+        <div id="calendar"></div>
+            @foreach($events as $event)
+                <div>{{$event->start->dateTime}}</div>
+            @endforeach
+        </div>
 
-            <div>{{$event->start->dateTime}}</div>
-
-
-        @endforeach
+        <div>
+            @foreach($events as $event)
+                <div>{{$event->start->dateTime}}</div>
+            @endforeach
+        </div>
     </div>
 </div>
 @endsection
