b555a2eb611b649719a18b9fe062d47199ade31f
Joseph Carmona <joseph.carmona@mdu.com>
2017-12-06 15:28:32 -0700

    Update Sass colors

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,21 @@
 @extends('layouts.app')
 
 @section('scripts')
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <title>Adopt-A-Meal</title>
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.7.0/fullcalendar.min.css' />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.19.2/moment.min.js"></script>
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.7.0/fullcalendar.min.js'></script>
+
     <script>
       $(document).ready(function() {
-        // page is now ready, initialize the calendar...
         $('#calendar').fullCalendar({
           showNonCurrentDates: false,
           contentHeight : "auto",
           height: 'parent' +80 ,
           aspectRatio: 1.5,
           themeSystem: 'bootstrap3'
-          // put your options and callbacks here
         });
       });
     </script>
@@ -20,16 +25,6 @@
 <div class="container">
     <div class="col-md-10 col-md-offset-2 calendar">
         <div id="calendar"></div>
-            @foreach($events as $event)
-                <div>{{$event->start->dateTime}}</div>
-            @endforeach
-        </div>
-
-        <div>
-            @foreach($events as $event)
-                <div>{{$event->start->dateTime}}</div>
-            @endforeach
-        </div>
     </div>
 </div>
 @endsection
