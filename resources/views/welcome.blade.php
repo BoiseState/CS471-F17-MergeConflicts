d1160b57d93f58251413c632c9a76ad03ada496e
Joseph Carmona <joseph.carmona@mdu.com>
2017-12-06 15:49:14 -0700

    Refactor!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,6 @@
 @extends('layouts.app')
 
 @section('scripts')
-
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <title>Adopt-A-Meal</title>
-        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.7.0/fullcalendar.min.css' />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.19.2/moment.min.js"></script>
-        <script src='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.7.0/fullcalendar.min.js'></script>
-
     <script>
       $(document).ready(function() {
         $('#calendar').fullCalendar({
@@ -19,7 +12,7 @@
         });
       });
     </script>
-    
+
 @endsection
 
 @section('content')
